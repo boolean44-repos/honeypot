19c550bfe4da4867e9c005a014c9bae1be611903
RiskyMH <git@riskymh.dev>
2026-04-14 03:04:12 +1000

    /honeypot experiments - lol up the max values to 6

--- src/events/interaction-create.ts
+++ src/events/interaction-create.ts
@@ -104,7 +104,7 @@ const handler: EventHandler<GatewayDispatchEvents.InteractionCreate> = {
                                     { label: "Random Channel Name (Chaos)", value: "random-channel-name-chaos", description: "Randomise the honeypot channel name with random characters (every day)", default: config.experiments.includes("random-channel-name-chaos") },
                                 ],
                                 min_values: 0,
-                                max_values: 5,
+                                max_values: 6,
                                 required: false,
                             }
                         }
