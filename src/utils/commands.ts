d6c130103094040a3476c6b3e180a0ff4771401e
RiskyMH <git@riskymh.dev>
2026-04-14 19:24:41 +1000

    update commands descriptions to have more keywords for searching (ie /setup)

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,10 +1,10 @@
-import { ApplicationCommandType, ApplicationIntegrationType, InteractionContextType, PermissionFlagsBits } from "discord-api-types/v10";
+import { ApplicationCommandType, ApplicationIntegrationType, InteractionContextType, PermissionFlagsBits, type RESTPutAPIApplicationCommandsJSONBody } from "discord-api-types/v10";
 
-export const commandsPayload = [
+export const commandsPayload: RESTPutAPIApplicationCommandsJSONBody = [
     {
         // this command opens a modal for configuring the honeypot
         name: "honeypot",
-        description: "Configure honeypot settings",
+        description: "Configure/setup the honeypot channel and its settings",
         type: ApplicationCommandType.ChatInput,
         options: [],
         default_member_permissions:
@@ -15,7 +15,7 @@ export const commandsPayload = [
     {
         // this command opens a modal for configuring the messages
         name: "honeypot-messages",
-        description: "Configure honeypot messages",
+        description: "Configure the honeypot messages that the bot sends",
         type: ApplicationCommandType.ChatInput,
         options: [],
         default_member_permissions:
@@ -25,7 +25,7 @@ export const commandsPayload = [
     },
     {
         name: "stats",
-        description: "See statistics all servers using honeypot",
+        description: "See statistics for all servers using honeypot",
         type: ApplicationCommandType.ChatInput,
         options: [],
         contexts: [InteractionContextType.BotDM],
